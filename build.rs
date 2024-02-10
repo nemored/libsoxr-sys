40b683ff2f51e55a99b7f74e101e28f0a24ab9bf
Noah Obert <2459563+nemored@users.noreply.github.com>
2024-02-09 16:55:00 -0800

    non-functioning env parsing

--- build.rs
+++ build.rs
@@ -3,17 +3,29 @@ extern crate pkg_config;
 fn main() {
     if std::env::var("DOCS_RS").is_err() {
         // do not probe for libsoxr when compiling at docs.rs
+        match std::env::var("SOXR_LINK_SEARCH") {
+            Ok(s) => {
+                println!("cargo:rustc-link-search={s}");
+                println!("cargo:rustc-link-lib=soxr");
+                println!("cargo:rerun-if-env-changed=SOXR_LINK_SEARCH");
+                println!("cargo:warning=bad bad bad");
+                return;
+            },
+            Err(_) => ()
+        }
+        println!("cargo:warning=didn't return yet");
         if let Err(e) = pkg_config::probe_library("soxr") {
             match e {
-            pkg_config::Error::Failure { .. } => panic! (
-                "Pkg-config failed - usually this is because libsoxr development headers are not installed.\n\n\
-                For Mac users using brew: brew install libsoxr\n\n\
-                For Debian/Ubuntu users:\n# apt-get install libsoxr0-dev\n\n\
-                pkg_config details:\n{}",
-                e
-            ),
-            _ => panic!("{}", e)
-        }
+                pkg_config::Error::Failure { .. } => panic! (
+                    "Pkg-config failed - usually this is because libsoxr development headers are not installed.\n\n\
+                     For Mac users using brew: brew install libsoxr\n\n\
+                     For Debian/Ubuntu users:\n# apt-get install libsoxr0-dev\n\n\
+                     pkg_config details:\n{}",
+                    e
+                ),
+                pkg_config::Error::EnvNoPkgConfig(_) => (),
+                _ => panic!("{}", e)
+            }
         }
     }
 }
